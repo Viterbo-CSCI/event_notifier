efada4a5d2a6409ffb2833ae23529008ab9733c4
ladew222 <egweinbe@gmail.com>
2025-04-11 10:17:31 -0500

    changed port on backend

--- backend/app.py
+++ backend/app.py
@@ -9,4 +9,4 @@ def hello():
     return {"message": "Hello from Flask!"}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5003, debug=True)
